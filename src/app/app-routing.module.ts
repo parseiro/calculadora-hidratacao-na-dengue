f919ae3c321e38ad2c38b983e878cd4fd3a9e6e1
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-05-01 03:14:51 -0300

    Link na navbar (e rota) para EventList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {PatientDataComponent} from "./patient-data/patient-data.component";
 import {CalculatedVolumeComponent} from "./calculated-volume/calculated-volume.component";
 import {AboutComponent} from "./about/about.component";
+import {EventListComponent} from "./event-list/event-list.component";
 
 const routes: Routes = [
   { path: 'patientData', component: PatientDataComponent  },
   { path: 'calculatedVolume', component: CalculatedVolumeComponent},
   { path: 'about', component: AboutComponent},
+  { path: 'eventList', component: EventListComponent},
   { path: '', redirectTo: 'patientData', pathMatch: 'full'}
 ];
 
