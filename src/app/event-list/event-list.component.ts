41f8211a8bff59bbe36d018bd1fb5018ef3283df
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-05-01 03:35:54 -0300

    Apresenta eventos usando interpolação e input

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-event-list',
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-list.component.scss']
 })
 export class EventListComponent implements OnInit {
+  eventos = [
+    {
+      id: 1,
+      nome: 'Conferência Anual de Dengue',
+      data: '25/9/2022'
+    },
+    {
+      id: 2,
+      nome: 'Conferência Nacional de Saúde',
+      data: '25/3/2023'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/event-thumbnail/event-thumbnail.component.ts
+++ src/app/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-event-thumbnail',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-thumbnail.component.scss']
 })
 export class EventThumbnailComponent implements OnInit {
+  @Input() event: any;
 
   constructor() { }
 
