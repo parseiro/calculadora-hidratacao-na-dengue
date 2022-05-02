a4b6c496a461591bee7e10226f178027cfd9424e
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-05-02 02:06:30 -0300

    Twitter meta tags in AboutComponent

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Title} from "@angular/platform-browser";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-about',
@@ -8,10 +8,20 @@ import {Title} from "@angular/platform-browser";
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private title: Title) { }
+  constructor(private title: Title,
+  private meta: Meta) { }
 
   ngOnInit(): void {
     this.title.setTitle("Sobre");
+
+    this.meta.updateTag({name: "description", content: "Sobre"});
+
+    this.meta.addTag({name: 'twitter:card', content: 'summary'});
+    this.meta.addTag({name: 'twitter:site', content: '@leopinheiro'});
+    this.meta.addTag({name: 'twitter:title', content: 'Sobre a calculadora de hidratação na dengue'});
+    // this.meta.addTag({name: 'twitter:description', content: 'summary'});
+    // this.meta.addTag({name: 'twitter:text:description', content: 'summary'});
+    // this.meta.addTag({name: 'twitter:image', content: 'summary'});
   }
 
 }
